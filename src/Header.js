8f2af6d305229377568507f0b36e022b23efa6da
Priyansh Maurya <priyansh.maurya@gmail.com>
2024-03-19 03:49:21 +0530

    fixes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { UpCircleOutlined } from "@ant-design/icons";
 import App from "./App";
 
 function Header({ title = "" }) {
   return (
     <div>
       <App />
-      {title} <UpCircleOutlined />
+      {title}
     </div>
   );
 }
